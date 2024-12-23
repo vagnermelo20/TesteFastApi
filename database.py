6752345d594aa05b4cf6998936d38139d8671569
Vagner Montenegro de Melo <vagnermontenegro20@gmail.com>
2024-12-23 11:31:12 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,4 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:2020@localh'
+URL_DATABASE = 'postgresql://postgres:2020@localhost:5432/QuizApplication'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+
+Base = declarative_base
