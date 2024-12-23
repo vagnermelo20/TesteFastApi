9ecaabcbb618f131507c9a53d16c3af91a47d546
Vagner Montenegro de Melo <vagnermontenegro20@gmail.com>
2024-12-23 11:30:00 -0300

    feito boa parte

---
+++ database.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = 'postgresql://postgres:2020@localh'
---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, HTTPException, Depends
+from  pydantic import BaseModel
+from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+class ChoiceBase (BaseModel):
+    choice_text: str
+    is_correct: bool
+
+class QuestionBase (BaseModel):
+    question_text: str
+    choices = List[ChoiceBase]
+
